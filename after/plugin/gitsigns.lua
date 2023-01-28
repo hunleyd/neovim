ed79d12fab9ade22b235d277541793993a8ee138
Douglas J Hunley <doug.hunley@gmail.com>
2023-01-28 09:53:48 -0500

    better gitsigns config

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,13 +1,14 @@
 require('gitsigns').setup {
     signs = {
-        add = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        change = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        add = { hl = "GitSignsAdd", text = "┃" },
+        change = { hl = "GitSignsChange", text = "┃" },
+        delete = { hl = "GitSignsDelete", text = "▁" },
+        topdelete = { hl = "GitSignsDelete", text = "▔" },
+        changedelete = { hl = "GitSignsChangeDelete", text = "┃" },
+        untracked = { hl = "GitSignsUntracked", text = "┃" },
     },
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-    numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+    numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
     linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
     word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
     watch_gitdir = {
@@ -28,7 +29,7 @@ require('gitsigns').setup {
     sign_priority = 6,
     update_debounce = 100,
     status_formatter = nil, -- Use default
-    max_file_length = 40000,
+    max_file_length = 100000,
     preview_config = {
         -- Options passed to nvim_open_win
         border = "single",
