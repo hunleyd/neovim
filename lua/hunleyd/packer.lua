8ea5d469bf7d05ed2b0363514295d378bb73d70a
Douglas J Hunley <doug.hunley@gmail.com>
2023-02-04 15:35:55 -0500

    force lspzero to 1.x branch

--- lua/hunleyd/packer.lua
+++ lua/hunleyd/packer.lua
@@ -1,7 +1,7 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
@@ -60,6 +60,7 @@ return require('packer').startup(function(use)
     -- LSP (via lsp-zero / mason)
     use {
         'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
         requires = {
             -- LSP Support
             { 'neovim/nvim-lspconfig' },
