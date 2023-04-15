59c0479e62859965944daadeef1b2ff46cbc3f7d
Douglas J Hunley <doug.hunley@gmail.com>
2023-04-15 12:28:04 -0400

    help->vimdoc

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "bash", "comment", "gitcommit", "gitignore", "help", "html", "lua", "markdown", "yaml" },
+  ensure_installed = { "bash", "comment", "gitcommit", "gitignore", "vimdoc", "html", "lua", "markdown", "yaml" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
