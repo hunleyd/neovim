ffc877c7addd8c7aa473fe248d418f617976f410
Douglas J Hunley <doug.hunley@gmail.com>
2023-04-30 09:23:30 -0400

    condigure lspzero v2

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,30 +1,44 @@
-local lsp = require('lsp-zero')
+# from https://www.reddit.com/r/neovim/comments/12wxwxs/lspzero_v2x_is_now_available/jhgskez/
+local lsp = require("lsp-zero").preset({})
 
-lsp.preset('recommended')
+lsp.ensure_installed({
+    "tsserver",
+    "rust_analyzer",
+})
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+    local opts = {buffer = bufnr, remap = false}
 
-  if client.name == "eslint" then
-      vim.cmd.LspStop('eslint')
-      return
-  end
-
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-  vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-  vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-  vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_vimdoc, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
+
+-- Fix Undefined global 'vim'
+require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
 
-vim.diagnostic.config({
-    virtual_text = true,
+local cmp = require("cmp")
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
+    sources = {
+        {name = "path"},
+        {name = "nvim_lsp"},
+        {name = "nvim_lua"},
+        {name = "buffer", keyword_length = 3},
+        {name = "luasnip", keyword_length = 2},
+    },
+    mapping = {
+        ["<C-Space>"] = cmp.mapping.complete(),
+    }
 })
 
