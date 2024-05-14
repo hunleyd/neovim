152fe5bf05e5051b58271357834763253eb947e1
Douglas J Hunley <doug.hunley@gmail.com>
2024-05-14 16:45:49 +0000

    feat(thanks): fix thanks.nvim setup
    Signed-off-by: Douglas J Hunley <doug.hunley@gmail.com>

---
+++ after/plugin/thanks.lua
@@ -0,0 +1,8 @@
+require("thanks").setup({
+	star_on_install = true,
+	star_on_startup = true,
+	ignore_repos = {},
+	ignore_authors = {},
+	unstar_on_uninstall = true,
+	ask_before_unstarring = false,
+})
