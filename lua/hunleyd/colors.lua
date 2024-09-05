9f9e4bfb981611ada916e7b591bd9b8513d9ca06
Douglas J Hunley <doug.hunley@gmail.com>
2024-09-05 23:17:34 +0000

    feat: switch to "flow" theme

--- lua/hunleyd/colors.lua
+++ lua/hunleyd/colors.lua
@@ -1,6 +1,12 @@
 function ColorMyPencils(color)
-	color = color or "cyberdream"
+	color = color or "flow"
 	vim.o.background = "dark"
+    require("flow").setup{
+        aggressive_spell = true,
+        fluo_color = 'yellow',
+        mode = "normal",
+        transparent = false,
+    }
 	vim.cmd.colorscheme(color)
 end
 ColorMyPencils()
