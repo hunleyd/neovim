2c69ac7a7938a66ac86f0ecf2e07c5b051f673e5
Douglas J Hunley <doug.hunley@gmail.com>
2026-06-13 04:06:47 +0000

    fix: statuscol sign rendering
    Updated statuscol.nvim configuration to use builtin.signfunc instead of the incorrect placeholder to correctly render diagnostic gutter signs.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1129,7 +1129,7 @@ function M.setup()
             relculright = true,
             segments = {
                 { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-                { text = { "%s" }, click = "v:lua.ScSa" },
+                { text = { builtin.signfunc }, click = "v:lua.ScSa" },
                 { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
             },
         })
